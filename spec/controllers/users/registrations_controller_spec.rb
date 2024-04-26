f03631de5f7ee3dabc1919b915cf507ed265ccb5
Enrique Rapela <enrique.rapela.developer@gmail.com>
2024-04-26 14:22:27 +0200

    fix: error in tests

--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Users::RegistrationsController, type: :request do
+RSpec.describe Users::RegistrationsController, type: :controller do
+  before do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+
   describe 'registrate a new user' do
     it 'with success response' do
-      post '/api/v1/signup', params: { user: { email: 'test@test.com', password: '123456' } }
+      post :create, params: { user: { email: 'test@test.com', password: '123456' } }
 
       expect(response).to have_http_status(:success)
     end
 
     it 'with invalid parameters' do
-      post '/api/v1/signup', params: { user: { email: 'testtest.com', password: '1234' } }
+      post :create, params: { user: { email: 'testtest.com', password: '1234' } }
 
       expect(response).to have_http_status(:unprocessable_entity)
     end
--- spec/controllers/users/sessions_controller_spec.rb
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -1,35 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Users::SessionsController, type: :request do
+RSpec.describe Users::SessionsController, type: :controller do
   let(:user){ create(:user) }
+
+  before do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
 
   describe 'loggin' do
     it 'with a correct user data' do
-      post '/api/v1/login', params: { user: { email: user.email, password: user.password } }
+      post :create, params: { user: { email: user.email, password: user.password } }
 
       expect(response).to have_http_status(:success)
     end
 
     it 'with invalid parameters' do
-      post '/api/v1/login', params: { user: { email: user.email, password: '1234' } }
-
-      expect(response).to have_http_status(:unauthorized)
-    end
-  end
-
-  describe 'logout' do
-    it 'with correct jwt' do
-      post '/api/v1/login', params: { user: { email: user.email, password: user.password } }
-      authorization = response.headers['authorization']
-      delete '/api/v1/logout', headers: { Authorization: authorization }
-
-      expect(response).to have_http_status(:ok)
-    end
-
-    it 'with invalid jwt' do
-      post '/api/v1/login', params: { user: { email: user.email, password: user.password } }
-
-      delete '/api/v1/logout', headers: { Authorization: 'invalid_token' }
+      post :create, params: { user: { email: user.email, password: '1234' } }
 
       expect(response).to have_http_status(:unauthorized)
     end
