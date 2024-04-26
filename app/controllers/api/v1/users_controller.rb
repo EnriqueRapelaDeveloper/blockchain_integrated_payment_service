541c13fb8effe6c83f3673f7ee3834d3f01f5ae7
Enrique Rapela <enrique.rapela.developer@gmail.com>
2024-04-26 18:30:22 +0200

    feat: puts another routes in controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   before_action :authenticate_user!
 
+  # GET /api/v1/me
   def index
     render json: UserSerializer.new(current_user), status: :ok
   end
