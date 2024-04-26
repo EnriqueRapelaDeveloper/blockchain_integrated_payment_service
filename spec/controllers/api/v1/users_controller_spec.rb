70bde55ab12e2273189a376df792ffe94acf6937
Enrique Rapela <enrique.rapela.developer@gmail.com>
2024-04-26 13:55:00 +0200

    feat: /me tests

---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::UsersController, type: :controller do
+  include Devise::TestHelpers
+  let(:user){ create(:user) }
+
+  describe 'index' do
+    it 'with a correct sign in' do
+      sign_in(user)
+
+      get :index
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'with a correct sign in' do
+      get :index
+
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+end
