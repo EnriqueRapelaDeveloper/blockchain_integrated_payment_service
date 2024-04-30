8c851e3e0ee2a1cdd4b7171677c2b0cde9be09ef
Enrique Rapela <enrique.rapela.developer@gmail.com>
2024-04-30 17:45:09 +0200

    feat: other case for test

--- spec/services/summary_service_spec.rb
+++ spec/services/summary_service_spec.rb
@@ -5,8 +5,12 @@ RSpec.describe FeeService, type: :model do
   let!(:fee_configuration) { create(:fee_configuration, user:, payments: false) }
   let!(:fiat_payment) { create(:fiat_payment, user:) }
 
+  let(:user2) { create(:user, email: 'test2@test.com') }
+  let!(:fee_configuration2) { create(:fee_configuration, user:, payments: false) }
+  let!(:fiat_payment2) { create(:fiat_payment, user: user2) }
+
   describe '#monthly_rates' do
-    it 'generate correct json' do
+    it 'generate correct json for user 1' do
       json = SummaryService.new(user).monthly_rates
 
       expect(json).to eq ({
@@ -26,6 +30,24 @@ RSpec.describe FeeService, type: :model do
             amount_currency: 'USDT'
           }
         ]
+      })
+    end
+
+    it 'generate correct json for user 2' do
+      json = SummaryService.new(user2).monthly_rates
+
+      expect(json).to eq ({
+        paid_fees: [
+          {
+            amount_cents: 199,
+            amount_currency: 'EUR'
+          },
+          {
+            amount_cents: 117,
+            amount_currency: 'USDT'
+          }
+        ],
+        unpaid_fees: []
       })
     end
   end
