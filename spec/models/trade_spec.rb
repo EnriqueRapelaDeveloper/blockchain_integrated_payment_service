8cd218cb36e9dde6828a06e7888505cc20ef7333
Enrique Rapela <enrique.rapela.developer@gmail.com>
2024-04-30 14:04:02 +0200

    feat: annotate spec models

--- spec/models/trade_spec.rb
+++ spec/models/trade_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: trades
+#
+#  id                       :bigint           not null, primary key
+#  final_amount_cents       :bigint
+#  final_amount_currency    :string
+#  original_amount_cents    :bigint
+#  original_amount_currency :string
+#  uuid                     :string           not null
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#  user_id                  :bigint           not null
+#
+# Indexes
+#
+#  index_trades_on_user_id  (user_id)
+#
 require 'rails_helper'
 
 RSpec.describe Trade, type: :model do
